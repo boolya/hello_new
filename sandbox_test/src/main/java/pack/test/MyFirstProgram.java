9b131fdcd8c3d17637318d55b70174d628ccf7e4
boolya <borisova.olga.383@gmail.com>
2017-08-13 22:03:28 +0700

    Добавлено несколько функций

--- sandbox_test/src/main/java/pack/test/MyFirstProgram.java
+++ sandbox_test/src/main/java/pack/test/MyFirstProgram.java
@@ -2,6 +2,17 @@ package pack.test;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello ("world");
+
+    double len = 5;
+    System.out.println("Площадь квадрата со сторной " + len + " = " + area(len));
+  }
+
+  public static void hello (String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double l) {
+    return l * l;
   }
 }
